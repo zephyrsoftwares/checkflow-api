9c40eeead7fc6f6fbc7117c8d803edc637eca22d
Ibrahim ADLANI <ibrahim@adlani.com>
2023-11-23 21:38:42 +0100

    Added AdminSite

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -34,3 +34,17 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.last_name)
         self.assertContains(res, self.user.phone_number)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
